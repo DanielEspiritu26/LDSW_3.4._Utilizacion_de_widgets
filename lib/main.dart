9cd530eb66cc3e0986fe1f6fa34655adbd5d1aad
DanielEspiritu26 <erick.espiritu23budgvirtual.udg.mx>
2026-04-24 10:09:39 -0600

    Home screen

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiApp());
+}
+
+class MiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+    
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://www.pexels.com/es-es/foto/primer-plano-de-palomitas-de-maiz-amarillas-recien-hechas-31427398/', 
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+        
+                Icon(
+                  Icons.movie,
+                  size: 80,
+                  color: Colors.white,
+                ),
+
+                SizedBox(height: 20),
+
+                
+                Text(
+                  'Catálogo de Películas',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

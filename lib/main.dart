b8f5093419ef05cc6dbfb904fef9b5af56ac62e2
DanielEspiritu26 <erick.espiritu23budgvirtual.udg.mx>
2026-04-24 11:47:45 -0600

    Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,40 +19,52 @@ class Inicio extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+
+         
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: NetworkImage(
-                  'https://images.pexels.com/photos/3142739/pexels-photo-3142739.jpeg',
+                  'https://images.pexels.com/photos/7640049/pexels-photo-7640049.jpeg', // puedes cambiarla
                 ),
                 fit: BoxFit.cover,
               ),
             ),
           ),
+
+      
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+
+             
                 Icon(
                   Icons.movie,
                   size: 80,
-                  color: Colors.white,
+                  color: Colors.black,
                 ),
+
                 SizedBox(height: 20),
+
+                
                 Text(
                   'Catálogo de Películas',
                   style: TextStyle(
                     fontSize: 28,
-                    color: Colors.white,
+                    color: Colors.black,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+
                 SizedBox(height: 20),
+
+            
                 Text(
                   'Hello World',
                   style: TextStyle(
                     fontSize: 22,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
               ],
@@ -63,4 +75,3 @@ class Inicio extends StatelessWidget {
     );
   }
 }
-``
